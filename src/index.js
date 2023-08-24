d7d7602b0fa661d4bdcf9009868ebf2be716a23b
ItzelQuintana <ovalli0326@gmail.com>
2023-08-24 17:31:14 -0600

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import analyzer from './analyzer.js';
-
+console.log("prueba");
+const botonr=document.querySelector("button");
+botonr.addEventListener("click", () =>{
+    document.getElementsByTagName("textarea")[0].value ="";
+  })
+ 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
