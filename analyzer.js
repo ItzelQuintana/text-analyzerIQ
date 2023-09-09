cf32d8faa2395c916264629870cbb06fde881927
ItzelQuintana <ovalli0326@gmail.com>
2023-09-09 01:47:15 -0600

    Updates

--- analyzer.js
+++ analyzer.js
@@ -1,27 +1,26 @@
 const analyzer = {
   getWordCount: (text) => {
-
-    const espacios = text.trim()
+    const espacios = text.trim();
     const texto = espacios.split(" ");
+    console.log(texto);
     let nPalabras = texto.length;
-    if (texto.length === 1 && texto[0] === "") {
+    if (texto[0] === "") {
       nPalabras = 0;
     }
-    
+
     return nPalabras;
 
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
-    const nCarac = text.length
+    const nCarac = text.length;
 
-    return nCarac
+    return nCarac;
 
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
-
-    const caracSE = text.replace(/\W/g, '').length
+    const caracSE = text.replace(/\W/g, "").length;
 
     return caracSE;
 
@@ -30,57 +29,50 @@ const analyzer = {
   getAverageWordLength: (text) => {
     const palabras = text.trim().split(" ");
     let sumaLongitudes = 0;
-    for (let i=0; i <palabras.length; i++){
-      sumaLongitudes +=palabras[i].length;
+    for (let i = 0; i < palabras.length; i++) {
+      sumaLongitudes += palabras[i].length;
+      
     }
-    const promedio = (sumaLongitudes/palabras.length).toFixed(2)
-    console.log(promedio)
+    const promedioSTR = (sumaLongitudes / palabras.length).toFixed(2);
+    const promedio = Number(promedioSTR);
+    console.log(promedio);
     return promedio;
 
-   //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
-    const nmsIdentificados = text.match(/-?\d+(\.\d+)?/g);
-    console.log(nmsIdentificados)
-    let nEnteros = 0;
-    let nDecimales = 0;
-    if (!nmsIdentificados) {
-      nEnteros = 0;
-      nDecimales = 0;
-    } else {
-      for (let i = 0; i < nmsIdentificados.length; i++) {
-      const numero = parseFloat(nmsIdentificados[i]);
-      if (Number.isInteger(numero)) {
-      nEnteros++;
-      }
-      else {
-      nDecimales++;
-      }
+    const elementosTotales = text.split(" ");
+    console.log(elementosTotales);
+    const elementosValidos = [];
+    for (let i = 0; i < elementosTotales.length; i++) {
+      const elementoV = elementosTotales[i].trim();
+      if (/^\d+(\.\d+)?$/g.test(elementoV)) {
+        elementosValidos.push(elementoV);
       }
     }
+    console.log(elementosValidos);
+    return elementosValidos.length;
 
-    console.log(nDecimales + nEnteros)
-    const nTotales = nDecimales + nEnteros;
-    return nTotales
-    
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
-    const nmsIngresados = text.match(/-?\d+(\.\d+)?/g)||[];
-    let arrayNms= [];
-    for (let i = 0; i < nmsIngresados.length; i++) {
-      arrayNms.push(Number(nmsIngresados[i]));
+    const arrayTotal = text.split(" ");
+    const arrayNms = [];
+    for (let i = 0; i < arrayTotal.length; i++) {
+      const numeroV = arrayTotal[i].trim();
+      if (/^\d+(\.\d+)?$/g.test(numeroV)) {
+        arrayNms.push(Number(numeroV));
+      }
     }
     console.log(arrayNms);
-  
+    let suma = 0;
+    for (let i = 0; i < arrayNms.length; i++) {
+      suma += arrayNms[i];
+    }
 
-    const suma = arrayNms.reduce((anterior, actual) => anterior + actual, 0);
     return suma;
-        
-   //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  }
-  
 
-
-}
+    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  },
+};
 export default analyzer;
